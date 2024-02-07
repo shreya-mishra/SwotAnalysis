db762cac4439286c158468bcfd743b22c91c6fc3
shreya-mishra <shreyamishra040398@gmail.com>
2024-02-07 12:43:15 +0530

    updated homescreen analyse button

--- src/components/Button_.tsx
+++ src/components/Button_.tsx
@@ -1,6 +1,5 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
-import {colors} from '../constants/Colors';
 
 type ButtonType = {
   title: string;
