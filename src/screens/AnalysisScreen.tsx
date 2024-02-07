053f92a5994084a13d423ef6fc6db86231ac34be
shreya-mishra <shreyamishra040398@gmail.com>
2024-02-07 12:41:49 +0530

    Added SwotGraph component

--- src/screens/AnalysisScreen.tsx
+++ src/screens/AnalysisScreen.tsx
@@ -1,54 +1,17 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import React from 'react';
 import {useRoute} from '@react-navigation/native';
-import {PieChart} from 'react-native-svg-charts';
+import SwotGraph from '../components/SwotGraph';
 
 const AnalysisScreen = () => {
   const route = useRoute();
 
   const {swotData} = route.params;
-  const strengthsCount = swotData.filter(
-    item => item.belongsTo === 'Strengths',
-  ).length;
-  const weaknessesCount = swotData.filter(
-    item => item.belongsTo === 'Weaknesses',
-  ).length;
-  const opportunitiesCount = swotData.filter(
-    item => item.belongsTo === 'Opportunities',
-  ).length;
-  const threatsCount = swotData.filter(
-    item => item.belongsTo === 'Threats',
-  ).length;
 
-  const transformedData = [
-    {
-      key: 1,
-      value: strengthsCount,
-      svg: {fill: 'green'},
-      arc: {outerRadius: '130%', cornerRadius: 10},
-    },
-    {
-      key: 2,
-      value: weaknessesCount,
-      svg: {fill: 'red'},
-      arc: {outerRadius: '110%', cornerRadius: 10},
-    },
-    {
-      key: 3,
-      value: opportunitiesCount,
-      svg: {fill: 'yellow'},
-      arc: {outerRadius: '90%', cornerRadius: 10},
-    },
-    {
-      key: 4,
-      value: threatsCount,
-      svg: {fill: 'orange'},
-      arc: {outerRadius: '70%', cornerRadius: 10},
-    },
-  ];
   return (
     <View style={styles.pieChartContainer}>
-      <PieChart style={{height: 300}} data={transformedData} />
+      {/* <PieChart style={{height: 300}} data={transformedData} /> */}
+      <SwotGraph swotData={swotData} />
     </View>
   );
 };
@@ -56,8 +19,8 @@ const AnalysisScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    backgroundColor: '#F5F5F5',
+    // padding: 16,
+    // backgroundColor: '#F5F5F5',
   },
   textInput: {
     height: 40,
